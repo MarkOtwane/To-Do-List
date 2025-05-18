eafc00b30bd781aebd1164c3be01f1b4aa31fd92
KING DAVID <otwanemark254@gmail.com>
2025-05-19 01:17:43 +0300

    create javascript  function to delete and update

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const table = document.querySelector("#tableData tbody");
 
 const task = JSON.parse(localStorage.getItem("task")) || [];
 addData();
+deleteData();
 
 form.addEventListener("submit", (ev)=>{
     ev.preventDefault();
@@ -19,9 +20,10 @@ form.addEventListener("submit", (ev)=>{
     };
 
     task.push(dataObject);//push it to the array
+    addData();
 
     localStorage.setItem("task", JSON.stringify(task));//set items /save items
-    addData();
+    
 
     form.reset();
 });
@@ -34,8 +36,29 @@ function addData(){
         formData.innerHTML = `
             <td>${dataObject.taskData}</td>
             <td>${dataObject.taskDate}</td>
-            <button id="submit">Submit</button>
+            <td><button class="edit-btn">Edit</button></td>
+            <td><button class="deleteData">Delete</button></td>
         `
-    })
-    table.appendChild(formData)
+        // append should be inside the loop
+        table.appendChild(formData)
+    });
+
+     // Function to remove an item from the scheduler
+     const removeBtn = document.querySelector(".deleteData");
+     removeBtn.addEventListener("click", () => {
+     formData.remove(); // remove the row
+     task.splice(index, 1); // remove the task from the array
+     localStorage.setItem("task", JSON.stringify(task));
+ });
+ const editBtn = document.querySelector(".edit-btn");
+ editBtn.addEventListener("click", ()=>{
+     // Create an in[put field] ide
+     const inputField = document.createElement("input")
+     // Identify which task is to be edited, replace the display text with an input containing the existing data, listen for a save of blur event, update the data on your local storage
+     inputField.type = "text";
+     formData.append(inputField);
+     // tasks.trim(index, 1)
+     localStorage.setItem("task", JSON.stringify(task));
+ });
+    
 }
